3f4589d16c4c52850338f4d93c0257dd2e6af716
Amit kumar <ramitgzp56@gmail.com>
2023-05-30 14:53:37 +0530

    project and projectcard

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import Particle from '../Particle'
 import{Container,Row,Col} from 'react-bootstrap'
+import ProjectCard from './ProjectCard'
 export default function Project() {
   return (
     <section>
-    <Container fluid className="about-section">
+    <Container fluid className="project-section">
       <Particle/>
-      <Container>
+      <Container className='project-content'>
       <Row>
-        <Col>
-        
+         <h1 >My Recent <strong className='purple'>Work</strong></h1>
+         <p style={{paddingBottom:"30px"}}>
+         Here are a few projects I've worked on recently and my published paper.
+         </p>
+         <hr />
+        <Col md={4}>
+       <ProjectCard />
         </Col>
-        <Col></Col>
+
+        <Col md={4}>
+        <ProjectCard />
+        </Col>
+
+        <Col md={4}>
+        <ProjectCard />
+        </Col>
+
+        <Col md={4}>
+        <ProjectCard />
+        </Col>
+
+        <Col md={4}>
+        <ProjectCard />
+        </Col>
+
+        <Col md={4}>
+        <ProjectCard />
+        </Col>
       </Row>
       </Container>
     </Container>
---
+++ src/components/Project/ProjectCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { CardGroup,Card } from 'react-bootstrap'
+import MyProfile from '../../Assets/myPortfolio.jpg'
+import { BiLinkExternal } from "react-icons/bi";
+
+export default function ProjectCard() {
+  return (
+    <CardGroup className='cardgroup'>
+    <Card  style={{background: 'transparent', }} className='cardelement'>
+        <Card.Img variant="top" src={MyProfile}  className='cardImage'/>
+       
+        <Card.Body>
+          <Card.Title style={{color:"white"}}>Portfolio</Card.Title>
+          <hr />
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <a href='#' target='_blank' className='project-links'>
+            < BiLinkExternal/> hbubjh</a>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+  )
+}
