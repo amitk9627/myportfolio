efb5ce97ddb1de785df7ec981bf756224342fb12
Amit kumar <ramitgzp56@gmail.com>
2023-08-12 18:33:46 +0530

    dfhtghf

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,45 +1,44 @@
 import React from 'react'
 import Particle from '../Particle'
-import{Container,Row,Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import ProjectCard from './ProjectCard'
+import myPortfolio from '../../Assets/myPortfolio.jpg'
+import shopping from '../..//Assets/shopping.jpg'
+import weather from '../../Assets/wheather.jpg'
+import tourIndia from '../../Assets/tourIndia.jpg'
+import movieApp from '../../Assets/movieApp.jpg'
 export default function Project() {
+  const projectData = [
+    { name: "Lets Shop", url: "https://64d77888bc78e77af1d0fb0a--chipper-panda-0c38e0.netlify.app/", image:shopping  },
+    { name: "My profile", url: "https://stellular-cobbler-b490aa.netlify.app/",image:myPortfolio },
+    { name: "Weather", url: "https://willowy-cascaron-c554fa.netlify.app/",image:weather },
+    { name: "Tour India", url: "https://dapper-buttercream-7c91d7.netlify.app/",image:tourIndia },
+    { name: "Movie App", url: "https://glittering-douhua-fd2b7b.netlify.app/",image:movieApp }
+  ];
   return (
     <section>
-    <Container fluid className="project-section">
-      <Particle/>
-      <Container className='project-content'>
-      <Row>
-         <h1 >My Recent <strong className='purple'>Work</strong></h1>
-         <p style={{paddingBottom:"30px"}}>
-         Here are a few projects I've worked on recently and my published paper.
-         </p>
-         <hr />
-        <Col md={4}>
-       <ProjectCard />
-        </Col>
+      <Container fluid className="project-section">
+        <Particle />
+        <Container className='project-content'>
+          <Row>
+            <h1 >My Recent <strong className='purple'>Work</strong></h1>
+            <p style={{ paddingBottom: "30px" }}>
+              Here are a few projects I've worked on recently and my published paper.
+            </p>
+            <hr />
+            {
+              projectData.map(
+                (item, key) => <Col md={4} key={key}>
+                                <ProjectCard item={item}/>
+                              </Col>
+              )
+            }
 
-        <Col md={4}>
-        <ProjectCard />
-        </Col>
 
-        <Col md={4}>
-        <ProjectCard />
-        </Col>
 
-        <Col md={4}>
-        <ProjectCard />
-        </Col>
-
-        <Col md={4}>
-        <ProjectCard />
-        </Col>
-
-        <Col md={4}>
-        <ProjectCard />
-        </Col>
-      </Row>
+          </Row>
+        </Container>
       </Container>
-    </Container>
-  </section>
+    </section>
   )
 }
--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -3,11 +3,12 @@ import { CardGroup,Card } from 'react-bootstrap'
 import MyProfile from '../../Assets/myPortfolio.jpg'
 import { BiLinkExternal } from "react-icons/bi";
 
-export default function ProjectCard() {
+export default function ProjectCard({item}) {
+  
   return (
     <CardGroup className='cardgroup'>
     <Card  style={{background: 'transparent', }} className='cardelement'>
-        <Card.Img variant="top" src={MyProfile}  className='cardImage'/>
+        <Card.Img variant="top" src={item.image}  className='cardImage'/>
        
         <Card.Body>
           <Card.Title style={{color:"white"}}>Portfolio</Card.Title>
@@ -19,8 +20,9 @@ export default function ProjectCard() {
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <a href='#' target='_blank' className='project-links'>
-            < BiLinkExternal/> hbubjh</a>
+          <a href={item.url} target='blank' className='project-links'>
+            < BiLinkExternal/> {item.name}
+          </a>
         </Card.Footer>
       </Card>
     </CardGroup>
